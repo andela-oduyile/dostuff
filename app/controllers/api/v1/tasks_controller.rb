b5bb3b9779d470ce55bdb1c289e5dffe423b4cd9
andela-oduyile <oluwatomi.duyile@andela.com>
2017-09-03 13:35:02 +0100

    implement crud action for task

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1
+    class TasksController < ApplicationController
+      def index
+        tasks = Task.order('created_at DESC')
+        render json: { success: true, data: tasks}, status: :ok
+      end
+
+      def show
+        task = Task.find(params[:id])
+        render json: { success: true, data: task}, status: :ok
+      end
+
+      def create
+        task = Task.new(task_params)
+
+        if task.save
+          render json: { success: true, data: task}, status: :ok
+        else
+          render json: { success: false, data: task.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        task = Task.find(params[:id])
+        if task.update(task_params)
+          render json: { success: true, data: task}, status: :ok
+        else
+          render json: { success: false, data: task.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        task = Task.find(params[:id])
+        task.destroy
+        render json: { success: true, data: task}, status: :ok
+      end
+
+      private
+        def task_params
+          params.permit(:content, :state)
+        end
+
+        def set_task
+        end
+    end
+  end
+end
